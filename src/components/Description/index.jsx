8c9180658a2f7887b95222deb46c7d586f5b4c85
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2022-12-22 16:28:04 +0200

    Create Description component

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,15 @@
+const Description = ({ descriptionField, handleDescriptionChange }) => {
+  return (
+    <div className="description-container">
+      <textarea
+        rows="50"
+        cols="4"
+        label="description"
+        value={descriptionField}
+        onChange={handleDescriptionChange}
+      />
+    </div>
+  );
+};
+
+export default Description;
--- src/pages/AppPage/index.jsx
+++ src/pages/AppPage/index.jsx
@@ -1,13 +1,25 @@
-import './app.css';
+import { useState } from "react";
+import Description from "../../components/Description";
+import "./app.css";
 
-const App = ()=> {
+
+const App = () => {
+  const [descriptionField, setDescriptionField] = useState("");
+  const descriptionOnChange = (event) => {
+    setDescriptionField(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Get in touch to order a punk beer </h1>
       </header>
+      <Description 
+        descriptionField={descriptionField}
+        handleDescriptionChange={descriptionOnChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
